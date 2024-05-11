eb74187eddf9324a738b6939d947a495fba2513d
yasin-arft <yasinarftsocial@gmail.com>
2024-05-11 17:52:49 +0600

    cors config

--- index.js
+++ index.js
@@ -19,7 +19,15 @@ const client = new MongoClient(uri, {
 });
 
 // middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://hunger-helper.web.app",
+    "https://hunger-helper.firebaseapp.com",
+  ],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -29,7 +37,7 @@ app.get('/', (req, res) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const foodCollection = client.db('hungerHelperDB').collection('foods');
 
@@ -50,7 +58,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
